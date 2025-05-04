e3d89e5dd04399891b33739487443b62d4c68603
Igor <igorabdallaaucar@hotmail.com>
2025-05-04 11:28:11 -0400

    Exercicio 01 VETORES

---
+++ ex01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 0, cont=0;
+    float nota[10], media, ac=0;
+
+    for (i=0; i<10 ; i++){
+        printf("Digite a sua nota: ");
+        scanf("%f", &nota[i]);
+        ac+= nota[i];
+    }
+
+    media = ac/10;
+
+    for (i=0; i<10;i++){
+        if(nota[i] > media){
+            cont++;
+        }
+    }
+
+
+    printf("A média da turma foi: %.2f\n", media);
+    printf("Número de alunos com nota acima da média: %d\n", cont);
+    
+    return 0;
+}
